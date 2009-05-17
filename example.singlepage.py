162beaaa75c5ef90282fce84883bdc959db1f1e9
Chris Hagerman <chris@webtreehouse.com>
2009-05-18 05:56:29 +0800

    rewrote as a python module to take advantage of python's threading and better memory management
    Signed-off-by: Chris Hagerman <chris@webtreehouse.com>

---
+++ example.singlepage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# rorrim
+#
+# Author: Chris Hagerman <chris@webtreehouse.com>
+# Copyright: Copyright (c) 2009 Chris Hagerman, released under the GPL.
+# License: GPL <http://www.gnu.org/licenses/gpl.html>
+# Version: 0.6
+#
+# This example shows how to save a single page and its images, css, and javascript to the output directory.
+
+import urlparse
+import rorrim
+
+s = rorrim.Site(source="http://github.com/", destination="output")
+print "Output has been saved to", s.primary_destination
